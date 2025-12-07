b1954a379ede3787ea95f823a43466ff7a0285de
Alamin <109461522+alaminmaih909@users.noreply.github.com>
2025-12-07 23:58:55 +0600

    Update property.js

--- src/lib/data/property.js
+++ src/lib/data/property.js
@@ -10,5 +10,5 @@ export const properties = Array.from({ length: 20 }, (_, i) => ({
   description: "Beautiful property with modern amenities and prime location.",
   images: ["/property.jpeg", "/property1.jpeg", "/property2.jpeg"],
   mapEmbed:
-    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46830151.11795828!2d-119.8093025!3d44.24236485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1765129901541!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade",
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46830151.11795828!2d-119.8093025!3d44.24236485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1765129901541!5m2!1sen!2sbd",
 }));
