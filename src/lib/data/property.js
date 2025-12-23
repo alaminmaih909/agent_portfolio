f3fa7896450beec8c51dcb1cc211c2412290e4b4
Alamin <developeralamin.web@gmail.com>
2025-12-23 21:16:02 +0600

    Updated

--- src/lib/data/property.js
+++ src/lib/data/property.js
@@ -8,7 +8,7 @@ export const properties = Array.from({ length: 20 }, (_, i) => ({
   bedrooms: 3,
   bathrooms: 2,
   area: "1500 sq.ft",
-  description: "Beautiful property with modern amenities and prime location."
+  description: "Beautiful property with modern amenities and prime location.",
   images: ["/property.jpeg", "/property1.jpeg", "/property2.jpeg"],
   mapEmbed:
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d46830151.11795828!2d-119.8093025!3d44.24236485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sbd!4v1765256574689!5m2!1sen!2sbd",
